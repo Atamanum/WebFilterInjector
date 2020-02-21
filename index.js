068d1dd118fce41786dd295deb443d5764b8c5fb
Igor <igortataru.94@gmail.com>
2020-02-22 00:36:55 +0100

    test

--- index.js
+++ index.js
@@ -1,1 +1,4 @@
-alert("aaa");
+//use that thing to import code into your browser
+$.loadScript('https://atamanum.github.io/WebFilterInjector/searcher.js', function(){
+    console.log("script loaded");
+});
