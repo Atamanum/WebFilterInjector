8e07b86f34d874749d0a550b43464e23af7d1e92
Igor <igortataru.94@gmail.com>
2020-02-22 01:06:21 +0100

    test

--- searcher.js
+++ searcher.js
@@ -2,7 +2,7 @@
 //maybe later will be upgraded to an universal strandart
 var atmFilter = function(bean)
 {
-	return parseFloat(bean[3])>3;
+	return parseFloat(bean[2])>3;
 };
 var runner = function()
 {
